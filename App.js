24c8fdcebc9affd5cf9b1250fbbb13b1fca9e00c
Ljubixonovich <ljubomir.vukelic@gmail.com>
2020-10-10 12:20:00 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>hackerNews</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
